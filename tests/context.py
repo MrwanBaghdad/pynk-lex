3fc000fbf7b5044d390da19b779f001619c11480
Marwan <mrwanbaghdad76@gmail.com>
2017-03-24 21:23:06 +0200

    corrected context.py

--- tests/context.py
+++ tests/context.py
@@ -1,5 +1,9 @@
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 import sys, os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
-import src
 
-print('hello world')
+logger.info('Start importing context')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__),'..')))
+import src.graph_builder, src.graph_mani
+logger.info('Finished import context')
