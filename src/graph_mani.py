7bb4d5b0b5b4bbe4ab6031f6073bd7e0204ec35b
Marwan <mrwanbaghdad76@gmail.com>
2017-03-24 16:22:18 +0200

    added higher order accepting func protoypes

--- src/graph_mani.py
+++ src/graph_mani.py
@@ -1,5 +1,5 @@
 from graph_builder import *
-
+import logging
 '''
 static module for graph manipulations and
 '''
@@ -10,7 +10,7 @@ def anding_two_graphs(graph_start, graph_end):
 
 def oring_two_graphs(graph1, graph2):
     '''
-    perfoming an or operation on two graphs
+    perfoming an OR operation on two graphs
     '''
     common_start = node()
     common_start.add_next_node(graph1.start_node)
@@ -19,3 +19,41 @@ def oring_two_graphs(graph1, graph2):
     graph1.final_node.add_next_node(common_end)
     graph2.final_node.add_next_node(common_end)
     return graph(common_start)
+
+def accepting_func_range(next_node, range_start, range_end):
+    '''
+        using closures a higher order function is returned that hide the next node
+        this function represents the vertices between two node
+    '''
+    def accepting_output(testing_char, master_key=False):
+        '''the higer order function with a master key flag
+        to allow accesing final node for graph class
+        accepting a range'''
+        if master_key:
+            return next_node
+        if testing_char >= range_start and testing_char <= range_end:
+            return next_node
+        else:
+            return None
+    return accepting_output
+
+def accepting_func_char(next_node, accept_char):
+    '''
+        using closures a higher order function is returned that hide the next node
+        this function represents the vertices between two node
+    '''
+    def accepting_output(testing_char, master_key=False):
+        '''the higer order function with a master key flag
+        to allow accesing final node for graph class
+        accepting a specfic char '''
+        if master_key:
+            return next_node
+        if testing_char == accept_char:
+            return next_node
+        else:
+            return None
+    return accepting_output
+
+def accpeting_func_elipson(next_node):
+    def accepting_output(testing_char, master_key=False):
+        return next_node
