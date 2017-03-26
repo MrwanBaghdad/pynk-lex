1b386935c093d94b1cd040bc9effbb705851e81e
Marwan <mrwanbaghdad76@gmail.com>
2017-03-26 20:21:37 +0200

    added convert to postfix

--- src/helpers.py
+++ src/helpers.py
@@ -14,7 +14,7 @@ def accepting_func_range(next_node, range_start, range_end):
         using closures a higher order function is returned that hide the next node
         this function represents the vertices between two node
     '''
-    def accepting_output(testing_char, master_key=False):
+    def accepting_output(testing_char=None, master_key=False):
         '''the higer order function with a master key flag
         to allow accesing final node for graph class
         accepting a range'''
@@ -31,7 +31,7 @@ def accepting_func_char(next_node, accept_char):
         using closures a higher order function is returned that hide the next node
         this function represents the vertices between two node
     '''
-    def accepting_output(testing_char, master_key=False):
+    def accepting_output(testing_char=None, master_key=False):
         '''the higer order function with a master key flag
         to allow accesing final node for graph class
         accepting a specfic char '''
@@ -45,7 +45,50 @@ def accepting_func_char(next_node, accept_char):
 
 def accpeting_func_elipson(next_node):
     '''accepting function for elipson char'''
-    def accepting_output(testing_char, master_key=False):
+    def accepting_output(testing_char=None, master_key=False):
         '''returned function accepts any char'''
         return next_node
     return accepting_output
+
+def to_postfix(str_in):
+    logging.info('start conversion to postfix')
+    postfix_stack = []
+    op_stack = []
+    precedence = {}
+    precedence['-'] = 3
+    precedence['+'] = 2
+    precedence['*'] = 2
+    precedence['|'] = 1
+    precedence['.'] = 1
+    precedence['('] = 0
+    for char in str_in:
+        if char  == '-':
+            op_stack.append(char)
+        elif char in ['[',']']:
+            pass
+        elif char in ['+','*','|','.','(']:
+            try:
+                if op_stack.__len__()  == 0:
+                    op_stack.append(char)
+                elif precedence[op_stack[-1]] > precedence[char] :
+                    while(precedence[op_stack[-1]] > precedence[char] or op_stack.__len__()>0):
+                        temp = op_stack.pop()
+                        postfix_stack.append(temp)
+                    op_stack.append(char)
+            except (IndexError, AttributeError, ValueError) as err:
+                logging.error(err)
+        elif char == ')':
+            temp = op_stack.pop()
+            while temp != '(' and op_stack.__len__()>0:
+                postfix_stack.append(temp)
+                temp = op_stack.pop()
+        else:
+            postfix_stack.append(char)
+        
+    while op_stack.__len__() > 0:
+        postfix_stack.append(op_stack.pop())
+    return ''.join(postfix_stack)
+
+if __name__ =='__main__':
+    #FIXME: this case
+    print(to_postfix('(a-b).(0-9)'))
