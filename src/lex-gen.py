b385e81e6becf4826812d6f02f8aa4216f7de615
Marwan <mrwanbaghdad76@gmail.com>
2017-03-28 15:37:48 +0200

    generate elipson table pre dfa gen

--- src/lex-gen.py
+++ src/lex-gen.py
@@ -1,5 +1,5 @@
 #TODO: read from lexical rule and make NFA
-
+#TODO: add logging
 import logging
 import os
 
@@ -25,11 +25,11 @@ def gen_great_NFA(input_lines):
             if literal_flag is True:
                 stk.append(char)
                 literal_flag = False
-            elif char in ['.','+','*','|','-']:
+            elif char in ['.', '+', '*', '|', '-']:
                 if char == '.':
                     c2 = stk.pop()
                     c1 = stk.pop()
-                    stk.append(anding_two_graphs(c1,c2))
+                    stk.append(anding_two_graphs(c1, c2))
 
                 elif char in ['*','+']:
                     #FIXME: make knlee and positive knlee
@@ -63,6 +63,48 @@ def gen_great_NFA(input_lines):
         super_node.add_next_node(accpeting_func_elipson(nfa.start_node))
     
     return super_node
+
+def gen_elipson_table(super_node):
+    '''ONE DFA TO RULE THEM ALL'''
+    all_nodes = []
+    all_nodes.append(super_node)
+    def get_all_node(super_node):
+        for node in super_node.get_next_node(master_key=True):
+            if node not in all_nodes:
+                all_nodes.append(node)
+                return get_all_node(node)
+            else:
+                return
+    get_all_node(super_node)
+    elipson_dict = []
+    node_temp = node()
+    elipson_dict[0] = accepting_func_char(node_temp,'c').__code__.co_code
+    elipson_dict[1] = accepting_func_range(node_temp, 0, 10).__code__.co_code
+    elipson_dict[2] = accpeting_func_elipson(node_temp).__code__.co_code
+
+    elipson_state_table = {}
+    def dfs_elipson_edges(starting_node_list, standing_node):
+        #use non locals
+        for edge in standing_node.next_node:
+            if edge.__code__.co_code == elipson_dict[2]:
+                if edge(master_key=True) not in starting_node_list:
+                    dfs_next_node= edge(master_key=True)
+                    starting_node_list.append(dfs_next_node)
+                    return dfs_elipson_edges(starting_node_list, dfs_next_node)
+            else: 
+                return
+
+    for node in all_nodes:
+        elipson_state_table[node] = list()
+        dfs_elipson_edges(elipson_state_table, node)
+    return elipson_state_table
+
+def gen_great_dfa(start_super_node, elipson_dict):
+    '''make lexical DFAs great again'''
+    
+    # get dict of all elipson moves
+    #get all nodes:
+    #TODO:
 
 
     # TODO: make postfix algo
