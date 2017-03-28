1d74cec28077432d0da7fbb2d5615ab75e988197
Marwan <mrwanbaghdad76@gmail.com>
2017-03-28 10:47:57 +0200

    added closure and positive closure

--- src/graph_mani.py
+++ src/graph_mani.py
@@ -6,25 +6,42 @@ static module for graph manipulations andq
 '''
 def anding_two_graphs(graph_start, graph_end):
     '''anding two graphs '''
-    graph_start.final_node.add_next_node(graph_end.start_node)
+    (graph1, graph2) = change_to_graphs(graph_start, graph_end)
+    graph1.end_node.add_next_node(graph2.start_node)
     return graph(graph_start.start_node)
 
 def oring_two_graphs(graph1, graph2):
     '''
     perfoming an OR operation on two graphs
     '''
+    (graph1,graph2) = change_to_graphs(graph1, graph2)
     common_start = node()
     common_start.add_next_node(accpeting_func_elipson(graph1.start_node))
     common_start.add_next_node(accpeting_func_elipson(graph2.start_node))
     common_end = node()
-    graph1.final_node.add_next_node(accpeting_func_elipson(common_end))
-    graph2.final_node.add_next_node(accpeting_func_elipson(common_end))
+    graph1.end_node.add_next_node(accpeting_func_elipson(common_end))
+    graph2.end_node.add_next_node(accpeting_func_elipson(common_end))
     return graph(common_start)
 
 def positive_knlee(graph):
     '''preformaing a posisitve knlee cloure on given graph'''
+    graph_in = change_to_graphs(graph)
     new_start = node()
     new_end = node()
+    new_start.add_next_node(accpeting_func_elipson(graph_in.start_node))
+    graph_in.end_node.add_next_node(accpeting_func_elipson(new_end))
+    graph_in.end_node.add_next_node(accpeting_func_elipson(graph_in.start_node))
+    return graph(new_start)
 
+def knlee(graph_inn):
+    '''preforming a knlee closure on graph'''
+    graph_in = change_to_graphs(graph_inn)
+    new_start = node()
+    new_end = node()
+    new_start.add_next_node(accpeting_func_elipson(graph_in.start_node))
+    graph_in.end_node.add_next_node(accpeting_func_elipson(new_end))
+    return graph(new_start)
+
+    
 if(__name__ == "main"):
     anding_two_graphs(graph(node()), graph(node()))
