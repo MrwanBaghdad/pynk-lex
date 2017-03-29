809cadc1840cf020559ac1621c1a1622627e0d2d
Marwan <mrwanbaghdad76@gmail.com>
2017-03-29 13:23:48 +0200

    preprocessing lex rules text file

---
+++ src/preproc.py
@@ -0,0 +1,30 @@
+import sys
+import os
+import re
+import logging
+import src.helpers as helpers
+logging.basicConfig(level=logging.INFO)
+class preproc():
+    def transfrom(lex_lines): #pylint: disable=E0213
+        logging.info("Started prep proc")
+        sym_table = {}
+        out = []
+        for i in lex_lines: #pylint: disable=E1133
+            if re.search(r'=',i) is not None:
+                temp = i.split('=')
+                if(sym_table.get(temp[0]) is not None):
+                    logging.error('two equivalent regular def')
+                    return None
+                temp[0] = temp[0].strip()
+                temp[1] = temp[1].strip()
+                sym_table[temp[0]] = temp[1]
+                logging.debug('added in sym table '+temp[0]+ ' '+sym_table[temp[0]])
+        logging.info('started translation')
+        for line in lex_lines: #pylint: disable=E1133
+            out.append(helpers.replace_all(line,sym_table))
+        logging.info(out)
+        logging.info('ended translation')
+        return out  
+
+if (__name__ == "__main__"):
+    preproc.transfrom(['x = [A | Z]','y =[3-12]', 'z: x + y'])
