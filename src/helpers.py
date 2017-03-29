3060d2522bab460a079f82121b905ab8a5509fc7
Marwan <mrwanbaghdad76@gmail.com>
2017-03-29 13:21:49 +0200

    tracing all inputs

--- src/helpers.py
+++ src/helpers.py
@@ -1,6 +1,9 @@
 import logging
 from .graph_builder import *
 logging.basicConfig(level=logging.INFO)
+
+inputs_range = []
+
 def replace_all(str_in,rep_dict):
     temp = str_in
     logging.info('replaceing string')
@@ -32,7 +35,7 @@ def change_to_graphs(inp1,inp2=None):
         start_node = node()
         start_node.add_next_node(accepting_func_char(node, inp2))
         graph2 = inp2
-    
+
     return (graph1, graph2)
 
 def accepting_func_range(next_node, range_start, range_end):
@@ -40,6 +43,8 @@ def accepting_func_range(next_node, range_start, range_end):
         using closures a higher order function is returned that hide the next node
         this function represents the vertices between two node
     '''
+    global inputs_range
+    inputs_range.append(('range', range_start, range_end))
     def accepting_output(testing_char=None, master_key=False):
         '''the higer order function with a master key flag
         to allow accesing final node for graph class
@@ -57,6 +62,8 @@ def accepting_func_char(next_node, accept_char):
         using closures a higher order function is returned that hide the next node
         this function represents the vertices between two node
     '''
+    global inputs_range
+    inputs_range.append(('char',accept_char))
     def accepting_output(testing_char=None, master_key=False):
         '''the higer order function with a master key flag
         to allow accesing final node for graph class
@@ -89,16 +96,16 @@ def to_postfix(str_in):
     precedence['.'] = 1
     precedence['('] = 0
     for char in str_in:
-        if char  == '-':
+        if char == '-':
             op_stack.append(char)
-        elif char in ['[',']']:
+        elif char in ['[', ']']:
             pass
-        elif char in ['+','*','|','.','(']:
+        elif char in ['+', '*', '|', '.', '(']:
             try:
-                if op_stack.__len__()  == 0:
+                if op_stack.__len__() == 0:
                     op_stack.append(char)
-                elif precedence[op_stack[-1]] > precedence[char] :
-                    while(precedence[op_stack[-1]] > precedence[char] or op_stack.__len__()>0):
+                elif precedence[op_stack[-1]] > precedence[char]:
+                    while precedence[op_stack[-1]] > precedence[char] or op_stack.__len__() > 0:
                         temp = op_stack.pop()
                         postfix_stack.append(temp)
                     op_stack.append(char)
@@ -106,7 +113,7 @@ def to_postfix(str_in):
                 logging.error(err)
         elif char == ')':
             temp = op_stack.pop()
-            while temp != '(' and op_stack.__len__()>0:
+            while temp != '(' and op_stack.__len__() > 0:
                 postfix_stack.append(temp)
                 temp = op_stack.pop()
         else:
@@ -119,3 +126,6 @@ def to_postfix(str_in):
 if __name__ =='__main__':
     #FIXME: this case
     print(to_postfix('(a-b).(0-9)'))
+    f1 =  accepting_func_char(node(), 'c')
+    assert f1.__code__.co_code == accepting_func_char(node(),'d').__code__.co_code
+    print(accpeting_func_elipson(node()).__code__.co_code)
